3167439ae2849ba504580756c2fe1b065952572e
Robert <robertbaggi@gmail.com>
2026-05-28 11:19:38 +0200

    feat: Update user management and Nginx configuration for improved backend integration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 set -e
 
+# Extract hostname from BACKEND_URL for proxy Host header
+export BACKEND_HOST=$(echo "$BACKEND_URL" | sed 's|https\?://||' | sed 's|/.*||')
+
 # Replace environment variables in nginx config template
-envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${BACKEND_URL} ${BACKEND_HOST}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;'
