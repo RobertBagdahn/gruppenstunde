21fe86a84b25c040e0a5f94ccff287033ae2e9c7
Robert <robertbaggi@gmail.com>
2026-06-23 22:26:48 +0200

    fix iphone bug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 set -e
-# Copy static config (no env substitution needed)
-cp /etc/nginx/conf.d/default.conf.template /etc/nginx/conf.d/default.conf
+
+BACKEND_URL="${BACKEND_URL:-https://inspi-backend-24xnoearra-ew.a.run.app}"
+BACKEND_HOST=$(echo "$BACKEND_URL" | sed 's|https://||' | sed 's|/.*||')
+
+export BACKEND_URL BACKEND_HOST
+
+envsubst '${BACKEND_URL} ${BACKEND_HOST}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
 exec nginx -g 'daemon off;'
